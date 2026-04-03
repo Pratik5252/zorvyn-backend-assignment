0f0ee983af7f206a322c5abbfa62a657f9d50d7d
Pratik <pratiky2000@gmail.com>
2026-04-03 16:56:28 +0530

    feat: set up express middleware for JSON parsing, cookie handling, and error management

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express from 'express';
-
+import cookieParser from 'cookie-parser';
+import { errorHandler } from './middleware/errorHandler';
+import authRouter from './modules/auth/auth.route';
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const router = express.Router();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+router.use('/auth', authRouter);
+
+app.use('/api/v1', router);
+
+app.use(errorHandler);
 
 export default app;
